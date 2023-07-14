6ae444c91f91005ad60dbaa2d8115bb4a2f2a2bc
Jayesh Singh <jayeshsingh62@gmail.com>
2023-07-14 15:47:44 +0530

    made the removepickup function blueprint callable

--- Source/LearningProject/Public/CPP_Character.h
+++ Source/LearningProject/Public/CPP_Character.h
@@ -28,7 +28,7 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	UFUNCTION(BlueprintCallable)
 	void RemovePickup();
 
 };
